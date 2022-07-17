26921ea2aa460c448d0d5624468e9e54d86e6231
Josef212 <jcasanovasv@hotmail.com>
2022-07-17 12:47:51 +0200

    Created payroll and response dto

---
+++ src/dtos/payrolls_dto.rs
@@ -0,0 +1,18 @@
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize, Debug, Default)]
+pub struct PayrollDto {
+    pub _id: i32,
+    pub date: String,
+    pub gross: f32,
+    pub net: f32,
+    pub ss: f32,
+    pub irpf: f32,
+    pub company_id: i32,
+    pub category_if: i32,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct PayrollsResponse {
+    pub payrolls: Vec<PayrollDto>,
+}
