067e5163cea6344f69424021cb65144e6f331e28
Josef212 <jcasanovasv@hotmail.com>
2022-07-20 19:06:09 +0200

    Missing file

---
+++ src/dtos/extra_data_dto.rs
@@ -0,0 +1,24 @@
+use std::collections::HashMap;
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize)]
+pub struct NamesIdsDto {
+    pub ids_to_names: HashMap<u32, String>,
+    pub names_to_ids: HashMap<String, u32>,
+}
+
+impl NamesIdsDto {
+    pub fn name_from_id(&self, id: u32) -> String {
+        match self.ids_to_names.get(&id) {
+            Some(name) => name.clone(),
+            None => "-Unknown name-".to_string(),
+        }
+    }
+    
+    pub fn id_from_name(&self, name: &String) -> u32 {
+        match self.names_to_ids.get(name) {
+            Some(id) => *id,
+            None => 0,
+        }
+    }
+}
