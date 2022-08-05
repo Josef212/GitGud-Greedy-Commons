6975b4e615c40b5e27c3c63187c4ba75c9817af4
Josef212 <jcasanovasv@hotmail.com>
2022-08-05 22:45:00 +0200

    Changed dtos from unsigned to signed

--- src/dtos/categories_dto.rs
+++ src/dtos/categories_dto.rs
@@ -2,7 +2,7 @@ use serde::{Serialize, Deserialize};
 
 #[derive(Serialize, Deserialize, Debug, Default)]
 pub struct CategoryDto {
-    pub _id: u32,
+    pub _id: i32,
     pub name: String,
     pub description: String,
 }
--- src/dtos/companies_dto.rs
+++ src/dtos/companies_dto.rs
@@ -2,7 +2,7 @@ use serde::{Serialize, Deserialize};
 
 #[derive(Serialize, Deserialize, Debug, Default)]
 pub struct CompanyDto {
-    pub _id: u32,
+    pub _id: i32,
     pub name: String,
     pub description: String,
 }
